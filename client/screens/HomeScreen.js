78f652d1ffc5506d2f7535ef8e9d2d09bcce799c
Pranav Dhingra <pranav.dhingra@gmail.com>
2018-05-19 19:05:35 -0500

    Revert "Implement comment prompt"

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -3,7 +3,6 @@ import {
   Image,
   Keyboard,
   KeyboardAvoidingView,
-  Modal,
   Picker,
   Platform,
   ScrollView,
@@ -20,7 +19,6 @@ import { MonoText } from '../components/StyledText';
 import { Col, Grid, Row } from 'react-native-easy-grid';
 import { Body, Button, Segment, Header, Title, Container, Content, Form, Textarea, H1, H2, H3 } from 'native-base';
 import MoodButton from '../components/MoodButton';
-import Prompt from 'react-native-prompt';
 /*
 Go to .node_modules/react-native-emoji/index.js and add
 import PropTypes from 'prop-types';
@@ -41,7 +39,6 @@ export default class HomeScreen extends ResponsiveComponent {
       activeShift: "Day",
       mood: 3,
       submitted: 0,
-      text: ""
     };
   }
   static navigationOptions = {
@@ -57,65 +54,49 @@ export default class HomeScreen extends ResponsiveComponent {
             <Title>IGNITE MoodTrack</Title>
           </Body>
         </Header>
-
+        //Try using KeyboardAvoidingView to push the text box into view. May be buggy.
         <ScrollView contentContainerStyle={constStyles.view} keyboardDismissMode="interactive">
-          <View style={style.subview}>
-            <H1>Position</H1>
-            <Segment style={style.segment}>
-              <Button active={this.state.activeJob === "Nurse"} first onPress={() => {this.setActiveJob("Nurse")}}><Text>Nurse</Text></Button>
-              <Button active={this.state.activeJob === "CNA"} onPress={() => {this.setActiveJob("CNA")}}><Text>CNA</Text></Button>
-              <Button active={this.state.activeJob === "Fellow"} onPress={() => {this.setActiveJob("Fellow")}}><Text>Fellow</Text></Button>
-              <Button active={this.state.activeJob === "Attending"} last onPress={() => {this.setActiveJob("Attending")}}><Text>Attending</Text></Button>
-            </Segment>
-          </View>
+            <View style={style.subview}>
+              <H1>Position</H1>
+              <Segment style={style.segment}>
+                <Button active={this.state.activeJob === "Nurse"} first onPress={() => {this.setActiveJob("Nurse")}}><Text>Nurse</Text></Button>
+                <Button active={this.state.activeJob === "CNA"} onPress={() => {this.setActiveJob("CNA")}}><Text>CNA</Text></Button>
+                <Button active={this.state.activeJob === "Fellow"} onPress={() => {this.setActiveJob("Fellow")}}><Text>Fellow</Text></Button>
+                <Button active={this.state.activeJob === "Attending"} last onPress={() => {this.setActiveJob("Attending")}}><Text>Attending</Text></Button>
+              </Segment>
+            </View>
 
-          <View style={style.subview}>
-            <H1>Location</H1>
-            <Segment style={style.segment}>
-              <Button active={this.state.activeLocation === "North"} first onPress={() => {this.setActiveLocation("North")}}><Text>North</Text></Button>
-              <Button active={this.state.activeLocation === "South"} last onPress={() => {this.setActiveLocation("South")}}><Text>South</Text></Button>
-            </Segment>
-          </View>
+            <View style={style.subview}>
+              <H1>Location</H1>
+              <Segment style={style.segment}>
+                <Button active={this.state.activeLocation === "North"} first onPress={() => {this.setActiveLocation("North")}}><Text>North</Text></Button>
+                <Button active={this.state.activeLocation === "South"} last onPress={() => {this.setActiveLocation("South")}}><Text>South</Text></Button>
+              </Segment>
+            </View>
 
-          <View style={style.subview}>
-            <H1>Shift</H1>
-            <Segment style={style.segment}>
-              <Button active={this.state.activeShift === "Day"} first onPress={() => {this.setActiveShift("Day")}}><Text>Day</Text></Button>
-              <Button active={this.state.activeShift === "Night"} last onPress={() => {this.setActiveShift("Night")}}><Text>Night</Text></Button>
-            </Segment>
-          </View>
+            <View style={style.subview}>
+              <H1>Shift</H1>
+              <Segment style={style.segment}>
+                <Button active={this.state.activeShift === "Day"} first onPress={() => {this.setActiveShift("Day")}}><Text>Day</Text></Button>
+                <Button active={this.state.activeShift === "Night"} last onPress={() => {this.setActiveShift("Night")}}><Text>Night</Text></Button>
+              </Segment>
+            </View>
 
-          <H1>How are you feeling today?</H1>
-          <View style={{flexDirection:'row'}}>
-            <Text style={constStyles.emoji}><Emoji name="disappointed"/></Text>
-            <Slider
-              style={constStyles.slider} value={this.state.mood}
-              minimumValue={1} maximumValue={5} onSlidingComplete={(val) => this.setState({mood:val})}>
-            </Slider>
-            <Text style={constStyles.emoji}><Emoji name="smile"/></Text>
-          </View>
-          <View>
-            <H1>Anything else?</H1>
-            <Prompt
-            title="Comment"
-            placeholder=""
-            defaultValue={ this.state.text }
-            visible={ this.state.promptVisible }
-            onCancel={ () => this.setState({
-              promptVisible: false,
-              text: "" // clears text on cancel
-            })}
-            onSubmit={ (value) => this.setState({
-              promptVisible: false,
-              text: value
-            })}
-            />
-
-            <Button style={ constStyles.promptButton } block onPress={() => this.setState({ promptVisible: true })}>
-              <Text>Add Comment</Text>
-            </Button>
-
-          </View>
+            <H1>How are you feeling today?</H1>
+            <View style={{flexDirection:'row'}}>
+              <Text style={constStyles.emoji}><Emoji name="disappointed"/></Text>
+              <Slider
+                style={constStyles.slider} value={this.state.mood}
+                minimumValue={1} maximumValue={5} onSlidingComplete={(val) => this.setState({mood:val})}>
+              </Slider>
+              <Text style={constStyles.emoji}><Emoji name="smile"/></Text>
+            </View>
+            <View style={{alignSelf:'stretch'}}>
+              <H1>Anything else?</H1>
+              <Form>
+                <Textarea rowSpan={5} bordered placeholder="Optional" />
+              </Form>
+            </View>
         </ScrollView>
         <MoodButton submitted={this.state.submitted}></MoodButton>
       </Container>
@@ -184,9 +165,5 @@ const constStyles = StyleSheet.create({
   view: {
     margin:25,
     paddingBottom:50
-  },
-  promptButton: {
-    backgroundColor:'#ffcc33',
-    marginVertical:25
   }
 });
