477d8577ba0234e018144547f1ce96d86698cd96
Khoa Truong <khoa.truong@u.northwestern.edu>
2018-05-22 14:38:35 -0500

    csv-feature works

--- server/routes/csv.js
+++ server/routes/csv.js
@@ -4,16 +4,18 @@ let router = express.Router();
 const Moods = require("../models/models").Moods;
 const json2csv = require("json2csv").parse;
 const fields = ["device_id", "user_type", "location","shift","timestamp", "mood", "comment"];
-const ops = { fields }
+const ops = { fields };
 
 /* GET moods in a csv. */
 router.get('/', function(req, res, next) {
   Moods.query()
-  .then(q => {
-    console.log(q) // debug - have a look to see what it looks like
-    let moods_data = json2csv(q, ops)
-    // res.status(200).attachment(moods_data).send("Downloading CSV snapshot of database.");
-    res.status(200).send("You're here.");
+  .then(data => {
+    // console.log(data) // debug - have a look to see what it looks like
+    let csv = json2csv(data, ops)
+    console.log(csv) // debug - have a look to see what it looks like
+    res.setHeader('Content-disposition', 'attachment; filename=moods.csv');
+    res.set('Content-Type', 'text/csv');
+    res.status(200).send(csv);
   })
   .catch(e => {
     console.error(e)
