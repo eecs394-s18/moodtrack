d66b57799e558c022f442a2d356621db0fbe1b93
Pranav Dhingra <pranav.dhingra@gmail.com>
2018-05-22 14:10:18 -0500

    Route works, need to implement json2csv logic

--- client/constants/ngrokUrls.js
+++ client/constants/ngrokUrls.js
@@ -1,1 +1,1 @@
-export default ['http://44849b5d.ngrok.io','https://44849b5d.ngrok.io']
+export default ['https://c4d71f1a.ngrok.io','http://c4d71f1a.ngrok.io']
--- server/routes/csv.js
+++ server/routes/csv.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+let router = express.Router();
 
-var Moods = require("../models/models").Moods;
-var json2csv = require("json2csv");
+const Moods = require("../models/models").Moods;
+const json2csv = require("json2csv").parse;
+const fields = ["device_id", "user_type", "location","shift","timestamp", "mood", "comment"];
+const ops = { fields }
 
 /* GET moods in a csv. */
 router.get('/', function(req, res, next) {
   Moods.query()
   .then(q => {
-    let fields = ["device_id", "user_type", "location","shift","timestamp", "mood", "comment"];
-    let moods_data = json2csv({/*...*/})
-    // res.status(200).attachment(moods_data);
+    console.log(q) // debug - have a look to see what it looks like
+    let moods_data = json2csv(q, ops)
+    // res.status(200).attachment(moods_data).send("Downloading CSV snapshot of database.");
     res.status(200).send("You're here.");
   })
   .catch(e => {
-    console.log(e)
+    console.error(e)
     res.status(204).send(e);
   });
 
